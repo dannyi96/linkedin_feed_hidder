7fbc8677ba8f1b6823b532ec651968ee37750d23
dannyi96 <danielbcbs2@gmail.com>
2023-07-30 04:55:03 +0530

    icons

--- src/content.js
+++ src/content.js
@@ -1,13 +1,12 @@
-// URL reference to target (change this to the URL you want to match)
 const URLS_TO_REMOVE = "https://www.linkedin.com/feed";
 const DEFAULT_URL_TO_CLICK = "https://www.linkedin.com/jobs/";
 
 function removeAnchorWithUrlReference(url) {
   const hyperlinks = document.querySelectorAll("a");
   hyperlinks.forEach((hyperlink) => {
-    // Check if the button's URL reference matches the target URL reference
+    // Check if the anchor's URL reference matches the target URL reference
     if (hyperlink.href && hyperlink.href.startsWith(url)) {
-      // Remove the button from the DOM
+      // Remove the anchor from the DOM
       hyperlink.remove();
     }
   });
