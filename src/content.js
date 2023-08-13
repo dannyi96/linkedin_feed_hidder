05520fa610c366c4271d52190c412fc8f6b497af
dannyi96 <danielbcbs2@gmail.com>
2023-08-14 02:33:33 +0530

    Redirect to jobs page only if social feed links are present on current page

--- src/content.js
+++ src/content.js
@@ -3,28 +3,40 @@ const DEFAULT_URL_TO_CLICK = "https://www.linkedin.com/jobs/";
 
 function removeAnchorWithUrlReference(url) {
   const hyperlinks = document.querySelectorAll("a");
+  var anchor_present = false;
+
   hyperlinks.forEach((hyperlink) => {
     // Check if the anchor's URL reference matches the target URL reference
     if (hyperlink.href && hyperlink.href.startsWith(url)) {
       // Remove the anchor from the DOM
       hyperlink.remove();
+      anchor_present = true;
     }
   });
+
+  return anchor_present;
 }
 
 function clickAnchorWithUrlReference(url) {
     const anchors = document.querySelectorAll("a");
     for (const anchor of anchors) {
-      if (anchor.href.startsWith(url)) {
+      if (anchor.href && anchor.href.startsWith(url)) {
+        // Click the first matching link and stop the loop
         anchor.click();
-        break; // Click the first matching link and stop the loop.
+        break; 
       }
     }
 }
 
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
-        removeAnchorWithUrlReference(URLS_TO_REMOVE);
-        clickAnchorWithUrlReference(DEFAULT_URL_TO_CLICK);
+        /* 
+          Remove all Social Feed URLs & redirect to jobs page
+          only if Social Feed URLs are present on current page
+        */
+        const anchor_found = removeAnchorWithUrlReference(URLS_TO_REMOVE);
+        if(anchor_found) {
+          clickAnchorWithUrlReference(DEFAULT_URL_TO_CLICK);
+        }
     }
 }
